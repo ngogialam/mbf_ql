e35195e049037fc8a284f1094125b3df2697614c
ngogialam <ngogialam98@gmail.com>
2023-03-10 15:47:09 +0700

    ddd

---
+++ logout.php
@@ -0,0 +1,21 @@
+<?php
+  // require "php/db_connection.php";
+  require "login.php";
+
+  if($con) {
+    $query = "UPDATE admin_credentials SET IS_LOGGED_IN = 'false'";
+    $result = mysqli_query($con, $query);
+  }
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Logout</title>
+    <script src="js/restrict.js"></script>
+  </head>
+  <body>
+
+  </body>
+</html>
