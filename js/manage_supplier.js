5cea01e855ce91c15d3c085be94034f5f7a3f59d
jandk-u <khanhp1406@gmail.com>
2023-03-23 10:00:50 +0700

    Update edit group sys

--- js/manage_supplier.js
+++ js/manage_supplier.js
@@ -10,9 +10,16 @@ function deleteSupplier(id) {
     xhttp.send();
   }
 }
+
+function viewEdit(id_team_sys) {
+  window.location.href = 'edit_team_sys.php?id_team_sys=' + id_team_sys;
+}
+
 function viewItem(id_team_sys) {
   window.location.href = 'detail_team_sys.php?id_team_sys=' + id_team_sys;
 }
+
+
 
 function goBack() {
   window.location.href = 'manage_supplier.php';
@@ -32,12 +39,13 @@ function updateSupplier(id) {
   var name_team_sys = document.getElementById("name_team_sys");
   var type = document.getElementById("type_sys");
   var describe = document.getElementById("describe_sys");
+  var create_by = document.getElementById("create_by");
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if(xhttp.readyState = 4 && xhttp.status == 200)
       document.getElementById('suppliers_div').innerHTML = xhttp.responseText;
   };
-  xhttp.open("GET", "php/manage_supplier.php?action=update&id_team_sys=" + id + "&name_team_sys=" + name_team_sys.value + "&type_sys=" + type.value +"&describe_sys=" + describe.value , true);
+  xhttp.open("GET", "php/manage_supplier.php?action=update&id_team_sys=" + id + "&name_team_sys=" + name_team_sys.value + "&type_sys=" + type.value +"&describe_sys=" + describe.value +"&create_by=" + create_by.value , true);
   xhttp.send();
   // }
 }
