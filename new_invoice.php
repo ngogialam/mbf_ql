e1ae2a9cde2a2219187b96a7ea30190616b8e66c
jandk-u <khanhp1406@gmail.com>
2023-04-03 15:54:53 +0700

    update feature sys_ql

--- new_invoice.php
+++ new_invoice.php
@@ -1,145 +1,223 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-  <head>
+
+<head>
     <meta charset="utf-8">
-    <title>Khai báo hệ thống mới</title>
+    <title> Chỉnh sửa hệ thống </title>
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
-		<script src="bootstrap/js/jquery.min.js"></script>
-		<script src="bootstrap/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
-    <link rel="shortcut icon" href="images/icon.svg" type="image/x-icon">
+    <script src="bootstrap/js/jquery.min.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css"
+        integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+    <link rel="shortcut icon" href="" type="image/x-icon">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="css/sidenav.css">
     <link rel="stylesheet" href="css/home.css">
-    <script src="js/suggestions.js"></script>
-    <script src="js/add_new_invoice.js"></script>
-    <script src="js/manage_invoice.js"></script>
     <script src="js/validateForm.js"></script>
     <script src="js/restrict.js"></script>
-  </head>
-  <body>
-    <div id="add_new_customer_model">
-      <div class="modal-dialog">
-      	<div class="modal-content">
-      		<div class="modal-header" style="background-color: #ff5252; color: white">
-            <div class="font-weight-bold">Khai báo hệ thống mới </div>
-      			<button class="close" style="outline: none;" onclick="document.getElementById('add_new_customer_model').style.display = 'none';"><i class="fa fa-close"></i></button>
-      		</div>
-      		<div class="modal-body">
-            <?php
-              include('sections/add_new_customer.html');
-            ?>
-      		</div>
-      	</div>
-      </div>
-    </div>
+    <script src="js/manage_invoice.js"></script>
+</head>
+
+<body>
     <!-- including side navigations -->
     <?php include("sections/sidenav.html"); ?>
-
     <div class="container-fluid">
-      <div class="container">
+        <div class="container" id="sys_div">
+            <!-- header section -->
+            <?php
+            require "php/header.php";
+            createHeader('user', 'Chỉnh sửa thông tin hệ thống', 'Thay đổi thông tin');
+            ?>
+            <div class="row">
+                <div class="row col col-md-12">
+                    <div class="row col col-md-12">
+                        <div class="col col-md-12 form-group">
+                            <label for="name_sys">Tên hệ thống :</label>
+                            <input id="name_sys" type="text" class="form-control" placeholder="tên hệ thống" >
+                        </div>
+                    </div>
+                    <div class="row col col-md-12" style="flex-direction: row-reverse;">
 
-        <!-- header section -->
-        <?php
-          require "php/header.php";
-          createHeader('clipboard', 'Khai báo hệ thống mới', 'Thêm hệ thống');
-        ?>
-        <!-- header section end -->
+                        <div class="col col-md-12 form-group">
+                            <label for="name_team_sys">Tên nhóm hệ thống :</label>
+                            <?php
+                            require "php/db_connection.php";
+                            $team_sys = "";
+                            if ($con) {
+                                $query = "SELECT * FROM team_sys_manager";
+                                $result = mysqli_query($con, $query);
 
-        <!-- form content -->
-        <div class="row">
-          <!-- customer details content -->
-          <div class="row col col-md-12">
-            <div class="col col-md-3 form-group">
-              <label class="font-weight-bold" for="customers_name">Tên nhóm hệ thống:</label>
-              <input id="customers_name" type="text" class="form-control" placeholder="Tên nhóm hệ thống" name="customers_name" onkeyup="showSuggestions(this.value, 'customer');">
-              <code class="text-danger small font-weight-bold float-right" id="customer_name_error" style="display: none;"></code>
-              <div id="customer_suggestions" class="list-group position-fixed" style="z-index: 1; width: 18.30%; overflow: auto; max-height: 200px;"></div>
-            </div>
-            <div class="col col-md-2 form-group">
-              <label class="font-weight-bold" for="invoice_number">Người tạo :</label>
-              <input id="invoice_number" type="text" class="form-control" name="created" placeholder="Invoice Number" >
-            </div>
-            <div class="col col-md-2 form-group">
-              <label class="font-weight-bold" for="">Loại :</label>
-              <select id="payment_type" class="form-control">
-              	<option value="1">Đầu tư</option>
-              	<option value="2">Hợp tác</option>
-              </select>
-            </div>
-            <div class="col col-md-2 form-group">
-              <label class="font-weight-bold" for="">Mô tả  :</label>
-              <input id="invoice_number" type="text" class="form-control" name="descriptionsư" placeholder="Mô tả" >
-            </div>
-            <div class="col col-md-2 form-group">
-              <label class="font-weight-bold" for="">Ngày tạo  :</label>
-              <input type="date" class="datepicker form-control hasDatepicker" id="invoice_date" value='<?php echo date('Y-m-d'); ?>' onblur="checkDate(this.value, 'date_error');">
-              <code class="text-danger small font-weight-bold float-right" id="date_error" style="display: none;"></code>
-            </div>
-          </div>
-          <!-- customer details content end -->
+                                echo '<select name="team_sys_manager" id="team_sys_manager" class=" form-control pdm chosen-select col col-md-12" >';
+                                while ($row = mysqli_fetch_assoc($result)) {
+                                    $id_team_sys = $row['id_team_sys'];
+                                    $name_team_sys = $row['name_team_sys'];
+                                    if ($id_team_sys == $id_team_sys_sys){
+                                        $team_sys = $name_team_sys;
+                                        echo "<option value= '$id_team_sys' selected='selected'>$name_team_sys</option>";
+                                    }
+                                    else
+                                        echo "<option value= '$id_team_sys' >$name_team_sys</option>";
+                                }
+                                echo '</select>';
+                            }
+                            ?>
+                        </div>
+                    </div>
+                    <div class="row col col-md-12">
+                        <div class="col col-md-12 form-group">
+                            <label for="first_number">Tên đầu số :</label>
+                            <input id="first_number" type="number" class="form-control"
+                                placeholder="tên đầu số"
+                            >
+                        </div>
+                    </div>
+                    <div class="row col col-md-12">
+                        <div class="col col-md-12 form-group">
+                            <label for="name_team_sys">Đơn vị quản lý :</label>
+                            <?php
+                            require "php/db_connection.php";
+                            if ($con) {
+                                $query = "SELECT * FROM unit_sys";
+                                $result = mysqli_query($con, $query);
+                                echo '<select name="unit_sys" id="unit_sys" class=" form-control pdm chosen-select col col-md-12" >';
+                                while ($row = mysqli_fetch_assoc($result)) {
+                                    $id_unit_sys = $row['id_unit_sys'];
+                                    $name_unit_sys = $row['name_unit_sys'];
+                                    
+                                    if ($id_unit_sys == $id_unit_sys_sys){
+                                        echo "<option value= '$id_unit_sys' selected='selected'>$name_unit_sys</option>";
+                                    }
+                                    else
+                                        echo "<option value='$id_unit_sys'>$name_unit_sys</option>";
+                                }
+                                echo '</select>';
+                            }
+                            ?>
+                        </div>
+                    </div>
+                    <div class="row col col-md-12">
+                        <div class="col col-md-12 form-group">
+                            <label for="name_team_sys">Đơn vị sử dụng :</label>
+                            <?php
+                            require "php/db_connection.php";
+                            if ($con) {
+                                $query = "SELECT * FROM unit_user";
+                                $result = mysqli_query($con, $query);
+                                echo '<select name="unit_user" id="unit_user" class=" form-control pdm chosen-select col col-md-12" >';
+                                while ($row = mysqli_fetch_assoc($result)) {
+                                    $id_unit_user = $row['id_unit_user'];
+                                    $name_unit_user = $row['name_unit_user'];
+                                    
+                                    if ($id_unit_user == $id_unit_user_sys){
+                                        echo "<option value= '$id_unit_user' selected='selected'>$name_unit_user</option>";
+                                    }
+                                    else
+                                        echo "<option value='$id_unit_user'>$name_unit_user</option>";
+                                }
+                                echo '</select>';
+                            }
+                            ?>
+                        </div>
+                    </div>
+                    <div class="row col col-md-12">
+                    <div class="col col-md-12 form-group">
+                            <label for="manager_user">Người quản lý :</label>
+                            <?php
+                            require "php/db_connection.php";
+                            if ($con) {
+                                $query = "SELECT * FROM manager_user";
+                                $result = mysqli_query($con, $query);
+                                echo '<select name="manager_user" id="manager_user" class=" form-control pdm chosen-select col col-md-12" >';
+                                while ($row = mysqli_fetch_assoc($result)) {
+                                    $id_user = $row['id_user'];
+                                    $name_user_manager = $row['name_user_manager'];
+                                    
+                                    if ($id_user == $id_user_manager_sys){
+                                        echo "<option value= '$id_user' selected='selected'>$name_user_manager</option>";
+                                    }
+                                    else
+                                        echo "<option value='$id_user'>$name_user_manager</option>";
+                                }
+                                echo '</select>';
+                            }
+                            ?>
+                        </div>
+                    </div>
+                    <div class="row col col-md-12">
+                        <div class="col col-md-12 form-group">
+                            <label for="name_team_sys">Mô tả hệ thống :</label>
+                            <input id="describe_sys" type="text" class="form-control" placeholder="mô tả hệ thống"
+                                 >
+                        </div>
+                    </div>
+                    <div class="row col col-md-12">
+                        <div class="col col-md-12 form-group">
+                            <label for="name_team_sys">Tài liệu hệ thống :</label>
+                            <input type="text" id="document_sys" class="form-control" 
+                                placeholder="tài liệu hệ thống"
+                                >
+                        </div>
+                    </div>
+                    <div class="row col col-md-12">
+                        <div class="col col-md-12 form-group">
+                            <label for="name_team_sys">Server hệ thống :</label>
+                            <input id="server_sys" type="text" class="form-control" 
+                                placeholder="server hệ thống"
+                                >
+                        </div>
+                    </div>
+                    <div class="row col col-md-12">
+                        <div class="col col-md-12 form-group">
+                            <label for="create_by">Người tạo:</label>
+                            <input id="create_by" type="number" class="form-control"
+                                placeholder="Người tạo">
+                        </div>
+                    </div>
+                    <div class="row col col-md-12">
+                        <div class="col col-md-12 form-group">
+                            <label for="name_team_sys">Ip hệ thống :</label>
+                            <input id="ip_sys" type="number" class="form-control"
+                                placeholder="ip hệ thống" >
+                        </div>
+                    </div>
+                    <div class="row col col-md-12">
+                        <div class="col col-md-12 form-group">
+                            <label for="name_team_sys">Cấu hình hệ thống :</label>
+                            <input id="config_sys" type="text" class="form-control" 
+                                placeholder="cấu hình hệ thống"
+                                >
+                        </div>
+                    </div>
 
-          <!-- new user button -->
-          <!-- <div class="row col col-md-12">
-            <div class="col col-md-2 form-group">
-              <button class="btn btn-primary form-control" onclick="document.getElementById('add_new_customer_model').style.display = 'block';">Thêm nhóm hệ thống</button>
+                    <div class="row col col-md-12" id="file_des_div" >
+                        <div class="col col-md-12 form-group">
+                            <label for="file">File mô tả :</label>
+                            <input id="file_des" type="file" name="file_des" />
+                        </div>
+                    </div>
+
+
+                    <!-- horizontal line -->
+                    <div class="col col-md-12">
+                        <hr class="col-md-12 float-left"
+                            style="padding: 0px; width: 95%; border-top: 2px solid  #02b6ff;">
+                    </div>
+
+                    <div class="row col col-md-12 m-auto"  >
+                        <div class="col col-md-2 form-group float-right"></div>
+                        <div id="update_button" class="col col-md-3 form-group float-right">
+                            <button class="btn btn-success form-control font-weight-bold"
+                                onclick="create();">Tạo mới</button>
+                        </div>
+                    </div>
+                    <!-- result message -->
+                    <div id="admin_acknowledgement" class="col-md-12 h5 text-success font-weight-bold text-center"
+                        style="font-family: sans-serif;"></div>
+                </div>
             </div>
-            <div class="col col-md-1 form-group"></div>
-          </div> -->
-          <!-- closing new user button -->
-
-          <div class="col col-md-12">
-            <hr class="col-md-12" style="padding: 0px; border-top: 3px solid  #02b6ff;">
-          </div>
-
-          <!-- add medicines -->
-          <div class="row col col-md-12">
-            <div class="row col col-md-12 font-weight-bold">
-              <div class="col col-md-2">Tên hệ thống</div>
-              <div class="col col-md-1">Đơn vị sử dụng</div>
-              <div class="col col-md-1">Đơn vị quản lý </div>
-              <div class="col col-md-1">Người quản lý</div>
-              <div class="col col-md-1">Đầu số</div>
-              <div class="col col-md-2">Mô tả hệ thống</div>
-              <div class="col col-md-1">Tài liệu sử dụng</div>
-              <div class="col col-md-1">Người tạo</div>
-              <div class="col col-md-1">Ngày tạo</div>
-              <div class="col col-md-1">Hành động </div>
-            </div>
-          </div>
-          <div class="col col-md-12">
-            <hr class="col-md-12" style="padding: 0px; border-top: 2px solid  #02b6ff;">
-          </div>
-
-          <div class="row col col-md-12 " id="invoice_medicine_list_div">
-            <script> addRow(); getInvoiceNumber(); </script>
-          </div>
-          <!-- end medicines -->
-          <div class="col col-md-12">
-            <hr class="col-md-12" style="padding: 0px;">
-          </div>
-
-          <div class="row col col-md-12">
-            <div id="save_button" class="col col-md-2 form-group float-right">
-              <label class="font-weight-bold" for=""></label>
-              <button class="btn btn-success form-control font-weight-bold" onclick="addInvoice();">Save</button>
-            </div>
-            <div id="new_invoice_button" class="col col-md-2 form-group float-right"  style="display: none;">
-              <label class="font-weight-bold" for=""></label>
-              <button class="btn btn-primary form-control font-weight-bold" onclick="location.reload();">New Invoice</button>
-            </div>
-            <div id="print_button" class="col col-md-2 form-group float-right" style="display: none;">
-              <label class="font-weight-bold" for=""></label>
-              <button class="btn btn-warning form-control font-weight-bold" onclick="printInvoice(document.getElementById('invoice_number').value);">Print</button>
-            </div>
-          </div>
-
-          <div id="invoice_acknowledgement" class="col-md-12 h5 text-success font-weight-bold text-center" style="font-family: sans-serif;"></div>
-
+            <hr style="border-top: 2px solid #ff5252;">
         </div>
-        <!-- form content end -->
-        <hr style="border-top: 2px solid #ff5252;">
-      </div>
     </div>
-  </body>
+</body>
+
 </html>
