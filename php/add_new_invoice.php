a40aa95970652bd2d3ec0b5b5be6ca5252b8be48
jandk-u <khanhp1406@gmail.com>
2023-04-04 17:08:12 +0700

    add feature he thong

--- php/add_new_invoice.php
+++ php/add_new_invoice.php
@@ -202,4 +202,332 @@
       echo ($result) ? "Invoice saved..." : "falied to add invoice...";
     }
   }
+
+
+
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $name_sys = $_POST["name_sys"];
+    if (empty($name_sys)) {
+      $mess = "Hệ thống đã tồn tại";
+      createForm($mess);
+      exit();
+    } 
+  
+    $team_sys_id = $_POST["team_sys_manager"];
+    $first_number = $_POST["first_number"];
+    $unit_sys = $_POST["unit_sys"];
+    $unit_user = $_POST["unit_user"];
+    $manager_user = $_POST["manager_user"];
+    
+    $describe_sys = $_POST["describe_sys"];
+    $document_sys = $_POST["document_sys"];
+    $describe_sys = $_POST["describe_sys"];
+    $server_sys = $_POST["server_sys"];
+    $ip_sys = $_POST["ip_sys"];
+    $config_sys = $_POST["config_sys"];
+    $create_by = $_POST["create_by"];
+
+    if(!empty($_FILES["file_des"])){
+      $file_des = basename($_FILES["file_des"]["name"]);
+    
+    // collect value of input field
+      $check = False;
+  
+      $target_dir = "../uploads/";
+      $target_file = $target_dir . basename($_FILES["file_des"]["name"]);
+      $uploadOk = 1;
+      $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+      
+      // Check if file already exists
+      if (file_exists($target_file)) {
+        ?>
+        <div class="row col col-md-12">
+          <div class="row col col-md-12">
+              <div class="col col-md-12 form-group">
+              <label for="id_team_sys">
+        <?php
+        echo "Thêm mới không thành công. File đã tồn tại.";
+        ?>
+        <label>
+          </div>
+          </div>
+          </div>
+        <?php
+        $uploadOk = 0;
+      }
+      
+      // Check file file_des
+      if ($_FILES["file_des"]["size"] > 5000000) {
+        ?>
+        <div class="row col col-md-12">
+          <div class="row col col-md-12">
+              <div class="col col-md-12 form-group">
+              <label for="id_team_sys">
+        <?php
+        echo "Thêm mới không thành cônng. File quá lớn";
+        ?>
+        <label>
+          </div>
+          </div>
+          </div>
+        <?php
+        $uploadOk = 0;
+      }
+      
+      // Allow certain file formats
+      if($imageFileType != "pdf" && $imageFileType != "doc" && $imageFileType != "ppt"
+      && $imageFileType != "xlsx" && $imageFileType != "docx" ) {
+        ?>
+        <div class="row col col-md-12">
+          <div class="row col col-md-12">
+              <div class="col col-md-12 form-group">
+              <label for="id_team_sys">
+        <?php
+        echo "Thêm mới không thành công. Chỉ upload file pdf, ppt, doc & xlsx.";
+        ?>
+        <label>
+          </div>
+          </div>
+          </div>
+        <?php
+        $uploadOk = 0;
+      }
+      
+      // Check if $uploadOk is set to 0 by an error
+      if ($uploadOk == 0) {
+        $mess = "Thêm mới file không thành công";
+        
+      // if everything is ok, try to upload file
+      } else {
+        if (move_uploaded_file($_FILES["file_des"]["tmp_name"], $target_file)) {
+          $mess= "Upload file ". htmlspecialchars( basename( $_FILES["file_des"]["name"])). " thành công";
+          createSys($team_sys_id, $unit_sys, $unit_user, $manager_user, $name_sys, $first_number, $describe_sys, $document_sys, $server_sys, $ip_sys, $config_sys, $create_by, $file_des);
+        } else {
+          $mess = "Thêm mới không thành công. Upload file không thành công";
+          createForm($mess);
+        }
+      }
+    }
+    else{
+      createSys($team_sys_id, $unit_sys, $unit_user, $manager_user, $name_sys, $first_number, $describe_sys, $document_sys, $server_sys, $ip_sys, $config_sys, $create_by, '');
+    }
+  }
+
+
+  function createSys($team_sys_id, $unit_sys, $unit_user, $manager_user, $name_sys, $first_number, $describe_sys, $document_sys, $server_sys, $ip_sys, $config_sys, $create_by, $file_des){
+    require "db_connection.php";
+    $query = "INSERT INTO sys_ql (unit_user_id, unit_sys_id, user_manager_id, team_sys_id, name_sys, first_number, describe_sys, document_sys, ip_sys, server_sys, config_sys, create_by, file_des)
+              VALUE ('$unit_user', '$unit_sys', '$manager_user', '$team_sys_id', '$name_sys', '$first_number', '$describe_sys', '$document_sys', '$ip_sys', '$server_sys', '$config_sys', '$create_by', '$file_des')";
+
+    $result = mysqli_query($con, $query);
+    if(!empty($result)){
+      $mess = "Thêm mới $name_sys thành công";
+      createForm($mess);
+    } else {
+      $mess = "Thêm mới $name_sys không thành công";
+      createForm($mess);
+    }
+      
+  }
+
+function createForm($mess){
+  ?>
+  <div class="row">
+    <div class="row col col-md-12">
+        
+        <div id="admin_acknowledgement" class="col-md-12 h5 text-success font-weight-bold text-center"
+            style="font-family: sans-serif;">
+                <?php
+                        echo $mess;
+                ?>
+        </div>
+        <div class="row col col-md-12">
+            <div class="col col-md-12 form-group">
+                <label for="name_sys">Tên hệ thống :</label>
+                <input id="name_sys" type="text" class="form-control" placeholder="tên hệ thống" >
+            </div>
+        </div>
+        <div class="row col col-md-12" style="flex-direction: row-reverse;">
+
+            <div class="col col-md-12 form-group">
+                <label for="name_team_sys">Tên nhóm hệ thống :</label>
+                <?php
+                require "db_connection.php";
+                $team_sys = "";
+                if ($con) {
+                    $query = "SELECT * FROM team_sys_manager";
+                    $result = mysqli_query($con, $query);
+
+                    echo '<select name="team_sys_manager" id="team_sys_manager" class=" form-control pdm chosen-select col col-md-12" >';
+                    while ($row = mysqli_fetch_assoc($result)) {
+                        $id_team_sys = $row['id_team_sys'];
+                        $name_team_sys = $row['name_team_sys'];
+                        if ($id_team_sys == $id_team_sys_sys){
+                            $team_sys = $name_team_sys;
+                            echo "<option value= '$id_team_sys' selected='selected'>$name_team_sys</option>";
+                        }
+                        else
+                            echo "<option value= '$id_team_sys' >$name_team_sys</option>";
+                    }
+                    echo '</select>';
+                }
+                ?>
+            </div>
+        </div>
+        <div class="row col col-md-12">
+            <div class="col col-md-12 form-group">
+                <label for="first_number">Tên đầu số :</label>
+                <input id="first_number" type="number" class="form-control"
+                    placeholder="tên đầu số"
+                >
+            </div>
+        </div>
+        <div class="row col col-md-12">
+            <div class="col col-md-12 form-group">
+                <label for="name_team_sys">Đơn vị quản lý :</label>
+                <?php
+                require "db_connection.php";
+                if ($con) {
+                    $query = "SELECT * FROM unit_sys";
+                    $result = mysqli_query($con, $query);
+                    echo '<select name="unit_sys" id="unit_sys" class=" form-control pdm chosen-select col col-md-12" >';
+                    while ($row = mysqli_fetch_assoc($result)) {
+                        $id_unit_sys = $row['id_unit_sys'];
+                        $name_unit_sys = $row['name_unit_sys'];
+                        
+                        if ($id_unit_sys == $id_unit_sys_sys){
+                            echo "<option value= '$id_unit_sys' selected='selected'>$name_unit_sys</option>";
+                        }
+                        else
+                            echo "<option value='$id_unit_sys'>$name_unit_sys</option>";
+                    }
+                    echo '</select>';
+                }
+                ?>
+            </div>
+        </div>
+        <div class="row col col-md-12">
+            <div class="col col-md-12 form-group">
+                <label for="name_team_sys">Đơn vị sử dụng :</label>
+                <?php
+                require "db_connection.php";
+                if ($con) {
+                    $query = "SELECT * FROM unit_user";
+                    $result = mysqli_query($con, $query);
+                    echo '<select name="unit_user" id="unit_user" class=" form-control pdm chosen-select col col-md-12" >';
+                    while ($row = mysqli_fetch_assoc($result)) {
+                        $id_unit_user = $row['id_unit_user'];
+                        $name_unit_user = $row['name_unit_user'];
+                        
+                        if ($id_unit_user == $id_unit_user_sys){
+                            echo "<option value= '$id_unit_user' selected='selected'>$name_unit_user</option>";
+                        }
+                        else
+                            echo "<option value='$id_unit_user'>$name_unit_user</option>";
+                    }
+                    echo '</select>';
+                }
+                ?>
+            </div>
+        </div>
+        <div class="row col col-md-12">
+        <div class="col col-md-12 form-group">
+                <label for="manager_user">Người quản lý :</label>
+                <?php
+                require "db_connection.php";
+                if ($con) {
+                    $query = "SELECT * FROM manager_user";
+                    $result = mysqli_query($con, $query);
+                    echo '<select name="manager_user" id="manager_user" class=" form-control pdm chosen-select col col-md-12" >';
+                    while ($row = mysqli_fetch_assoc($result)) {
+                        $id_user = $row['id_user'];
+                        $name_user_manager = $row['name_user_manager'];
+                        
+                        if ($id_user == $id_user_manager_sys){
+                            echo "<option value= '$id_user' selected='selected'>$name_user_manager</option>";
+                        }
+                        else
+                            echo "<option value='$id_user'>$name_user_manager</option>";
+                    }
+                    echo '</select>';
+                }
+                ?>
+            </div>
+        </div>
+        <div class="row col col-md-12">
+            <div class="col col-md-12 form-group">
+                <label for="name_team_sys">Mô tả hệ thống :</label>
+                <input id="describe_sys" type="text" class="form-control" placeholder="mô tả hệ thống"
+                      >
+            </div>
+        </div>
+        <div class="row col col-md-12">
+            <div class="col col-md-12 form-group">
+                <label for="name_team_sys">Tài liệu hệ thống :</label>
+                <input type="text" id="document_sys" class="form-control" 
+                    placeholder="tài liệu hệ thống"
+                    >
+            </div>
+        </div>
+        <div class="row col col-md-12">
+            <div class="col col-md-12 form-group">
+                <label for="name_team_sys">Server hệ thống :</label>
+                <input id="server_sys" type="text" class="form-control" 
+                    placeholder="server hệ thống"
+                    >
+            </div>
+        </div>
+        <div class="row col col-md-12">
+            <div class="col col-md-12 form-group">
+                <label for="create_by">Người tạo:</label>
+                <input id="create_by" type="number" class="form-control"
+                    placeholder="Người tạo">
+            </div>
+        </div>
+        <div class="row col col-md-12">
+            <div class="col col-md-12 form-group">
+                <label for="name_team_sys">Ip hệ thống :</label>
+                <input id="ip_sys" type="number" class="form-control"
+                    placeholder="ip hệ thống" >
+            </div>
+        </div>
+        <div class="row col col-md-12">
+            <div class="col col-md-12 form-group">
+                <label for="name_team_sys">Cấu hình hệ thống :</label>
+                <input id="config_sys" type="text" class="form-control" 
+                    placeholder="cấu hình hệ thống"
+                    >
+            </div>
+        </div>
+
+        <div class="row col col-md-12" id="file_des_div" >
+            <div class="col col-md-12 form-group">
+                <label for="file">File mô tả :</label>
+                <input id="file_des" type="file" name="file_des" />
+            </div>
+        </div>
+
+
+        <!-- horizontal line -->
+        <div class="col col-md-12">
+            <hr class="col-md-12 float-left"
+                style="padding: 0px; width: 95%; border-top: 2px solid  #02b6ff;">
+        </div>
+
+        <div class="row col col-md-12 m-auto"  >
+            <div class="col col-md-2 form-group float-right"></div>
+            <div id="update_button" class="col col-md-3 form-group float-right">
+                <button class="btn btn-success form-control font-weight-bold"
+                    onclick="create();">Tạo mới</button>
+            </div>
+        </div>
+        <!-- result message -->
+        <div id="admin_acknowledgement" class="col-md-12 h5 text-success font-weight-bold text-center"
+            style="font-family: sans-serif;"></div>
+    </div>
+</div>
+<?php
+}
+
 ?>
